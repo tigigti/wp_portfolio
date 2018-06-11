cf1e9c24bea71f3710938f57f2eadf1d5eec9985
tigigti <angelos.ioannou@gmx.de>
2018-06-11 22:57:22 +0200

    Display only "blog_post" category on blog

--- home.php
+++ home.php
@@ -3,12 +3,19 @@
 <div class="blog-container">
    <h1>Blog</h1>
     <?php
-    if(have_posts()):
-        while(have_posts()):
-            the_post();
-            the_content();
+    $query_blogposts = new WP_Query( 'category_name=blog_post' );
+    if($query_blogposts->have_posts()):
+        while($query_blogposts->have_posts()):
+            $query_blogposts->the_post();
+            the_title();
+            echo get_the_date();
+            ?>
+            <a href="<?php the_permalink();?>">To Post</a>
+            <?php
+            the_excerpt();
         endwhile;
     endif;
+    wp_reset_postdata();
     ?> 
 </div>
 
