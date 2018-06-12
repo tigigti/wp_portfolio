284a3594f893d085a8a31cf5bf482483f88599d6
Angelos Ioannou <angelos.ioannou@gmx.de>
2018-06-12 19:14:08 +0200

    new markup for blog posts

--- home.php
+++ home.php
@@ -1,18 +1,22 @@
 <?php get_header(); ?>
 <div class="sky-unit"></div>
-<div class="blog-container">
+<div id="main-content" class="blog-container">
    <h1>Blog</h1>
     <?php
     $query_blogposts = new WP_Query( 'category_name=blog_post' );
     if($query_blogposts->have_posts()):
         while($query_blogposts->have_posts()):
             $query_blogposts->the_post();
-            the_title();
-            echo get_the_date();
             ?>
-            <a href="<?php the_permalink();?>">To Post</a>
+            <div class="blog-post">
+                <a href="<?php the_permalink();?>" class="post-title"><?php the_title();?></a>
+                <h3 class="post-date">&mdash; <?php echo get_the_date();?></h3>
+                <p class="post-content">
+                    <?php the_excerpt();?>
+                </p>
+            </div>
+            
             <?php
-            the_excerpt();
         endwhile;
     endif;
     wp_reset_postdata();
