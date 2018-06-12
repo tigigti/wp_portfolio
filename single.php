bd75211c7e0c74e4832879bec0bd12936ba18d3c
Angelos Ioannou <angelos.ioannou@gmx.de>
2018-06-12 22:32:32 +0200

    rewrote markup

--- single.php
+++ single.php
@@ -1,34 +1,30 @@
-<?php
-/**
- * The template for displaying all single posts and attachments
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
- 
-get_header(); ?>
-    <div class="sky-unit"></div>
-    <div class="blog-container">
- 
-        <?php
-        // Start the loop.
-        while ( have_posts() ) : the_post();
- 
-            the_title();
-            the_content();
- 
-            // Previous/next post navigation.
-            the_post_navigation( array(
-                'prev_text' => "Letzter Post: .%title",
-                'next_text' => "Nächster Post: .%title",
-                "taxonomy"  => "category",
-                "in_same_term" => true
-            ) );
- 
-        // End the loop.
-        endwhile;
-        ?>
-    </div><!-- .content-area -->
+<?php get_header(); ?>
+<div class="sky-unit"></div>
+<div class="blog-container">
+
+    <?php
+    // Start the loop.
+    while ( have_posts() ) : the_post();
+    ?>
+        <div class="blog-post single">
+            <h2 class="post-title"><?php the_title();?></h2>
+            <h3 class="post-date">&mdash; <?php echo get_the_date();?></h3>
+            <p class="post-content">
+                <?php the_excerpt();?>
+            </p>
+        </div>
+    <?php
+        // Previous/next post navigation.
+        the_post_navigation( array(
+            'prev_text' => "Letzter Post: %title",
+            'next_text' => "Nächster Post: %title",
+            "taxonomy"  => "category",
+            "in_same_term" => true
+        ) );
+
+    // End the loop.
+    endwhile;
+    ?>
+</div>
  
 <?php get_footer(); ?>
